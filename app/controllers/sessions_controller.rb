6648e888d87cc70d08cc348e5e913fd9c90a64c5
Yo Kamada <bumpfuji10@gmail.com>
2022-03-04 08:22:57 +0900

    ログイン中とログアウト中のヘッダー表示の違い

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,15 @@
 class SessionsController < ApplicationController
 
-  def Show
-    @user = User.find(params[:id])
-  end
-
-
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email.downcase])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
-      # エラーメッセージを作成する
+      flash.now[:danger] = "ログインに失敗しました"
     render 'new'
     end
   end
